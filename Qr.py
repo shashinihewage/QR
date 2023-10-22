2444e016fd479efcc23fda90ecbbb7b12b8d8936
shashinihewage <shashinihansika2000@gamil.com>
2023-10-22 11:08:42 +0530

    test 2

--- Qr.py
+++ Qr.py
@@ -8,4 +8,3 @@ feature.add_data("https://github.com/shashinihewage") #add data/link to qr code
 feature.make(fit=True)
 generate_img = feature.make_image(fill="black", back_color="white") #qr code color can change according to the user preference
 generate_img.save("qr.png")
-
